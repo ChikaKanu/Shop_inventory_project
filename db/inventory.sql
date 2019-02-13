3624d18cadfba75c0207b4f922fd9afd03b67249
Ifeyinwa Kanu <chika.kanu@procompletions.com>
2019-02-13 22:15:40 +0000

    CSS enhancement manufacturer 7

--- db/inventory.sql
+++ db/inventory.sql
@@ -18,8 +18,8 @@ CREATE TABLE products (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
   description VARCHAR(255),
-  manufacturer_id INT8 REFERENCES manufacturers(id),
-  category_id INT8 REFERENCES categories(id),
+  manufacturer_id INT8 REFERENCES manufacturers(id) ON DELETE CASCADE,
+  category_id INT8 REFERENCES categories(id) ON DELETE CASCADE,
   quantity INT4,
   cost INT4,
   price INT4
